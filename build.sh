35162929a9bc7377d6818cd85889cdd080a71a6d
KiquarSL <io.dmitry.akimov@gmail.com>
2026-09-09 21:01:28 +0700

    Added Catppuccin Mocha theme

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+t#!/bin/bash
 # Build all projects with nodejs
-projects=("itsaky" "feslake" "github" "darcula" "tokyo-night")
+projects=("itsaky" "feslake" "github" "darcula" "tokyo-night" "catppuccin-mocha")
 
 for i in "${projects[@]}"; do
 	cp build.js "$i"
